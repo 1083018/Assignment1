1b039814436fac5fe3bb52cd15de86175277e43b
1085931 <146641218+1085931@users.noreply.github.com>
2023-10-01 17:19:39 +0400

    Update Try.java

--- Try.java
+++ Try.java
@@ -1,6 +1,7 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("How much is "+number1+" times "+number2+"?");
+    System.out.println("The result is: " + (number1 * number2));
   }
 }
 
